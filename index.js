cb9f3b142844d5e7404b2cf7e43046af76710c7c
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-12-09 22:33:52 -0600

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const server = express();
 
-const port = process.env.PORT || 9000;
+const port = process.env.port || 9000;
 
 server.get('/', (req, res) => {
   res.json({ api: 'Deploy me' });
